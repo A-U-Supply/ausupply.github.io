f3430c0daba32e074fc6eae484db7a3a08e2c48a
jake <doo-nothing@users.noreply.github.com>
2026-02-11 13:45:08 -0800

    revert debug logging to INFO level

--- puke-box/scrape_midieval.py
+++ puke-box/scrape_midieval.py
@@ -331,7 +331,7 @@ def run_scraper() -> int:
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
     exit(run_scraper())
